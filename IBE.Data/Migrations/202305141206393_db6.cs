26d9e2a7af939fe75ecc9b37b8ced74f750e490e
home <75668578@qq.com>
2023-05-14 20:08:17 +0800

    over

---
+++ IBE.Data/Migrations/202305141206393_db6.cs
@@ -0,0 +1,16 @@
+﻿namespace IBE.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class db6 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
