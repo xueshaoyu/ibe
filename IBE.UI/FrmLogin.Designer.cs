d78b6c8f26f781e2c992f77e28d1393b0c11c02a
home <75668578@qq.com>
2023-05-14 19:41:58 +0800

    aa

--- IBE.UI/FrmLogin.Designer.cs
+++ IBE.UI/FrmLogin.Designer.cs
@@ -83,6 +83,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(86, 106);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(242, 21);
             this.textBox2.TabIndex = 3;
             // 
